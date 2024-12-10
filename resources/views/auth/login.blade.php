a8251479abb02823b20b732e9279470a20579ab6
Deni Suryadi <deni.w4f@gmail.com>
2024-12-10 14:35:08 +0700

    Update baground login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,7 +56,7 @@
             <!-- /Left Text -->
             <div class="d-none d-lg-flex col-lg-7 p-0">
                 <div class="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
-                    <img src="{{asset('vuexy/assets/img/illustrations/auth-login-illustration-light.png')}}" alt="auth-login-cover" class="img-fluid my-5 auth-illustration" data-app-light-img="illustrations/auth-login-illustration-light.png" data-app-dark-img="illustrations/auth-login-illustration-dark.png" />
+                    <img src="{{asset('vuexy/assets/img/bg-silat.png')}}" alt="auth-login-cover" class="img-fluid my-5 auth-illustration" data-app-light-img="illustrations/auth-login-illustration-light.png" data-app-dark-img="illustrations/auth-login-illustration-dark.png" />
 
                     <img src="{{asset('vuexy/assets/img/illustrations/bg-shape-image-light.png')}}" alt="auth-login-cover" class="platform-bg" data-app-light-img="illustrations/bg-shape-image-light.png" data-app-dark-img="illustrations/bg-shape-image-dark.png" />
                 </div>
@@ -68,14 +68,9 @@
                 <div class="w-px-400 mx-auto">
                     <!-- Logo -->
                     <div class="app-brand mb-4">
-                        <a href="index.html" class="app-brand-link gap-2">
-                            <span class="app-brand-logo demo">
-                                <svg width="32" height="22" viewBox="0 0 32 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z" fill="#7367F0" />
-                                    <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd" d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z" fill="#161616" />
-                                    <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd" d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z" fill="#161616" />
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z" fill="#7367F0" />
-                                </svg>
+                        <a href="{{ route('login') }}" class="logo logo-light text-center">
+                            <span class="logo-lg">
+                                <img src="{{asset('vuexy/assets/img/logo-dark-cn.png')}}" alt="">
                             </span>
                         </a>
                     </div>
@@ -110,6 +105,10 @@
                         <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
                         <!-- <button class="btn btn-primary" type="submit">Log In </button> -->
                     </form>
+                    <div class="mt-5 text-center">
+                        <p>Belum Punya Akun ? <a href="{{ route('register') }}" class="font-weight-medium text-primary"> Register </a> </p>
+                        <p>© {{ now()->year }} Crafted with <i class="mdi mdi-heart text-danger"></i> by Cakra Niskala</p>
+                    </div>
 
                     <!-- <p class="text-center">
                         <span>New on our platform?</span>
@@ -142,6 +141,27 @@
     </div>
 
     <!-- / Content -->
+
+    <!-- Authentication js -->
+    <script src="{{asset('ubold/assets/js/pages/authentication.init.js')}}"></script>
+    <script src="{{asset('lib/sweetalert2/sweetalert2.js')}}"></script>
+
+
+    @if($message)
+    <script>
+        setTimeout(function() {
+            Swal.fire({
+                icon: `error`,
+                title: `Login Failed`,
+                html: 'Kombinasi username dan password tidak sesuai',
+                showCancelButton: false,
+                showConfirmButton: true,
+                confirmButtonColor: "#DD6B55",
+                // timer: 2000,
+            })
+        }, 1000);
+    </script>
+    @endif
 
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
